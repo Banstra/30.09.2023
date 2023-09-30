3e4f5259518c2f6ab0ba0a5152c168f6fcd965e4
Akagami no Shanks <89997083+MustafaAlibekov@users.noreply.github.com>
2023-09-30 09:26:15 +0300

    30.09.2023

---
+++ Pages/Authorization.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TurAgency.pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Page
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Authorize(object sender, RoutedEventArgs e)
+        {
+            if(Login.Text == "root" && Password.Text == "12345")
+            {
+                classes.Manager.MainFrame.Navigate(new Admin());
+            }
+            if (Login.Text == "user" && Password.Text == "12345")
+            {
+                classes.Manager.MainFrame.Navigate(new User());
+            }
+        }
+    }
+}
---
+++ Pages/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TurAgency
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new pages.Authorization());
+            classes.Manager.MainFrame = MainFrame;
+        }
+
+        private void AuthorizeClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
